8159b99b7b3459fb09fb64573433198652a0f614
xuwenqi320@163.com <xuwenqi320@163.com>
2018-04-26 11:17:50 +0800

    把交易Tx 也放到RabbitMQ队列里面了

--- queue_blocks_in_order.js
+++ queue_blocks_in_order.js
@@ -1,5 +1,5 @@
 var amqp = require('amqp');
-//var BigNumber = require("bignumber.js");
+var BigNumber = require("bignumber");
 var winston = require("winston");
 var Web3 = require("web3");
 // 连接到以太坊节点
@@ -14,7 +14,7 @@ else
 
 
 var	 BLOCK_REWARD		 = 5,
-	 FIRST_BLOCK		 = 0,
+	 FIRST_BLOCK		 = 500000,
 	 MAXIMUM_BLOCK		 = 1000000,
 	 REPORT_FREQUENCY_BLOCKS = 1800;
 
@@ -31,7 +31,23 @@ var queue_blocks_from = function (block_number) {
 			console.log("error", "Error getting block number: " + block_number + ". '" + error + "'");
 		} else {
 			if(result){
-		console.log("info", "Queueing Block #" + block_number + " (with " + result.transactions.length + " transactions).");
+				//保存交易
+				var cur_tx,cur_tx_type;
+				for(var i=0 ; i<result.transactions.length;i++){
+					cur_tx = result.transactions[i];
+					 if(cur_tx.to == null){
+                                                cur_tx_type = 2; // Contract Creation
+                                        } else {
+                                                if(web3.eth.getCode(cur_tx.to) == "0x"){
+                                                        cur_tx_type = 0; // Person to Person
+                                                } else {
+                                                        cur_tx_type = 1; // Person to Contract
+                                                }
+                                        }
+                                        console.log(cur_tx,cur_tx_type)
+					connection.publish("ethtx",cur_tx)
+					connection.publish("ethtx",cur_tx_type)
+ 				}
 				if(block_number % REPORT_FREQUENCY_BLOCKS == 0){ 
 					console.log("info", "Queueing Block #" + block_number + " (with " + result.transactions.length + " transactions).");
 				}
