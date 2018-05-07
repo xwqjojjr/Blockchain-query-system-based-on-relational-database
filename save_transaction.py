4425a8f246e7a1b9a5d665c188e40566406d506a
xuwenqi320@163.com <xuwenqi320@163.com>
2018-05-07 20:12:12 +0800

    忘记修改了什么...

--- save_transaction.py
+++ save_transaction.py
@@ -9,7 +9,35 @@ import binascii
 from util import Helper
 
 # mysql语句 
-insert_into_block      = "INSERT INTO ethereum.Transactions(block_number,block_hash,tx_from,tx_to,tx_hash,tx_index,tx_input,tx_value,tx_type,nonce) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+insert_into_Transactions      = "INSERT INTO ethereum.Transactions(block_number,block_hash,tx_from,tx_to,tx_hash,tx_index,tx_input,tx_value,tx_type,nonce) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+insert_into_Tx_To = "INSERT INTO ethereum.Tx_To(addr,tx_hash) VALUES (%s,%s)"
+insert_into_Tx_From =  "INSERT INTO ethereum.Tx_From(addr,tx_hash) VALUES (%s,%s)"
+
+'''
+CREATE TABLE `Transactions` (
+  `block_number` int(11) DEFAULT NULL,
+  `block_hash` varchar(100) DEFAULT NULL,
+  `tx_from` varchar(100) DEFAULT NULL,
+  `tx_hash` varchar(100) DEFAULT NULL,
+  `tx_index` int(11) DEFAULT NULL,
+  `tx_input` varchar(10000) DEFAULT NULL,
+  `tx_value` varchar(100) DEFAULT NULL,
+  `tx_type` int(11) DEFAULT NULL,
+  `nonce` int(11) DEFAULT NULL,
+  `tx_to` varchar(100) DEFAULT NULL
+);
+
+drop table Tx_From;
+create table Tx_From(
+addr varchar(70),
+tx_hash varchar(70)
+);
+drop table Tx_To;
+create table Tx_To(
+addr varchar(70),
+tx_hash varchar(70)
+);
+'''
 
 # 每次汇报的频率
 FREQUENCY = 100
@@ -39,6 +67,7 @@ def callback(ch, method, properties,body):
     global count
 #    print len(body)
     if(len(body) > 100):
+#	print(tx)
     	tx = json.loads(body)
         count = count +1
     else: 
@@ -54,12 +83,17 @@ def callback(ch, method, properties,body):
 def save_tx(tx,tx_type):
     insert_into_tx_args    = [ tx["blockNumber"],tx["blockHash"],tx["from"],tx["to"],tx["hash"],tx["transactionIndex"],tx["input"],tx["value"],tx_type,tx["nonce"] ]
     try:
-        cursor.execute(insert_into_block,insert_into_tx_args)
-        if(tx["blockNumber"] % FREQUENCY == 0):
+        cursor.execute(insert_into_Transactions,insert_into_tx_args)
+#        db.commit()
+	cursor.execute(insert_into_Tx_From,[tx["from"],tx["hash"]])
+#	db.commit()
+	cursor.execute(insert_into_Tx_To,[tx["to"],tx["hash"]])
+#        print( db.commit())
+	if(tx["blockNumber"] % FREQUENCY == 0):
             db.commit()
             print("Loading tx to DB from #%s (at %s )"% (tx["blockNumber"], time.asctime(time.localtime())  ))
     except Exception as e:
-        print("ERROR",str(e),insert_into_block)
+        print("ERROR",str(e))
 #   开始监听
 channel.basic_consume(callback , queue=BLOCK_QUEUE_NAME , no_ack=False)
 channel.start_consuming()
