69ef14c4f44c96237d90386c2b194bb33ce0117e
xuwenqi320@163.com <xuwenqi320@163.com>
2018-05-30 19:03:10 +0800

    修改了表结构和保存代码

--- save_transaction.py
+++ save_transaction.py
@@ -9,35 +9,7 @@ import binascii
 from util import Helper
 
 # mysql语句 
-insert_into_Transactions      = "INSERT INTO ethereum.Transactions(block_number,block_hash,tx_from,tx_to,tx_hash,tx_index,tx_input,tx_value,tx_type,nonce) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-insert_into_Tx_To = "INSERT INTO ethereum.Tx_To(addr,tx_hash) VALUES (%s,%s)"
-insert_into_Tx_From =  "INSERT INTO ethereum.Tx_From(addr,tx_hash) VALUES (%s,%s)"
-
-'''
-CREATE TABLE `Transactions` (
-  `block_number` int(11) DEFAULT NULL,
-  `block_hash` varchar(100) DEFAULT NULL,
-  `tx_from` varchar(100) DEFAULT NULL,
-  `tx_hash` varchar(100) DEFAULT NULL,
-  `tx_index` int(11) DEFAULT NULL,
-  `tx_input` varchar(10000) DEFAULT NULL,
-  `tx_value` varchar(100) DEFAULT NULL,
-  `tx_type` int(11) DEFAULT NULL,
-  `nonce` int(11) DEFAULT NULL,
-  `tx_to` varchar(100) DEFAULT NULL
-);
-
-drop table Tx_From;
-create table Tx_From(
-addr varchar(70),
-tx_hash varchar(70)
-);
-drop table Tx_To;
-create table Tx_To(
-addr varchar(70),
-tx_hash varchar(70)
-);
-'''
+insert_into_Transactions      = "INSERT INTO ethereum.Transactions(blockNumber,blockHash,txHash,txFrom,txTo,txIndex,txInput,txValue,txType) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
 # 每次汇报的频率
 FREQUENCY = 100
@@ -81,14 +53,9 @@ def callback(ch, method, properties,body):
 #   在数据库中保存交易
 
 def save_tx(tx,tx_type):
-    insert_into_tx_args    = [ tx["blockNumber"],tx["blockHash"],tx["from"],tx["to"],tx["hash"],tx["transactionIndex"],tx["input"],tx["value"],tx_type,tx["nonce"] ]
+    insert_into_tx_args    = [ tx["blockNumber"],tx["blockHash"],tx["hash"],tx["from"],tx["to"],tx["transactionIndex"],tx["input"],tx["value"],tx_type ]
     try:
         cursor.execute(insert_into_Transactions,insert_into_tx_args)
-#        db.commit()
-	cursor.execute(insert_into_Tx_From,[tx["from"],tx["hash"]])
-#	db.commit()
-	cursor.execute(insert_into_Tx_To,[tx["to"],tx["hash"]])
-#        print( db.commit())
 	if(tx["blockNumber"] % FREQUENCY == 0):
             db.commit()
             print("Loading tx to DB from #%s (at %s )"% (tx["blockNumber"], time.asctime(time.localtime())  ))
