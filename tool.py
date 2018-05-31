41f639b6ddd202ea56e21c09cc833501b86c6ce1
xuwenqi320@163.com <xuwenqi320@163.com>
2018-05-31 22:19:40 +0800

    加了注释

--- tool.py
+++ tool.py
@@ -1,7 +1,8 @@
 import sha3
 import rlp
 import binascii
-
+#求智能合约地址
+#发送地址+nonce进行rlp编码，再用keccak_256哈希计算，保留后160位，再转化为2进制
 sha3.keccak_256(rlp.encode([binascii.unhexlify("b100987e77feba6f022dbd22b80b008e35d3ff2e"),0])).hexdigest()[-40:]
 class Helper(object):
 
