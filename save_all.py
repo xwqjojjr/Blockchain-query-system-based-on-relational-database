f5bc80e725fd7f4c1a93fd7a16736242bb121430
xuwenqi320@163.com <xuwenqi320@163.com>
2018-05-31 22:16:26 +0800

    求智能合约地址原来是错误的，现在已经改正了，在tool.py

--- save_all.py
+++ save_all.py
@@ -6,7 +6,7 @@ import json
 import rlp
 import sha3
 import binascii
-from util import Helper
+from tool import Helper
 helper = Helper()
 # mysql语句               
 insert_into_block      = "INSERT INTO ethereum.EthTest_blocks(blockNumber,blockHash,timeStamp,prevBlockHash,minerAddr,txCount,gasUsed) VALUES (%s,%s,FROM_UNIXTIME(%s),%s,%s,%s,%s)"
@@ -15,7 +15,7 @@ insert_into_AddressHistory    = "INSERT INTO EthTest_addresshistory(blockNumber,
 insert_into_addr 			  = "INSERT INTO EthTest_address(addrHash,addrType) VALUES(%s,%s)"
 
 # 每次汇报的频率
-FREQUENCY = 2
+FREQUENCY = 100
 
 # RABBITMQ信息,创建connection和channel
 RABBIT_INFO = {
@@ -50,51 +50,65 @@ def is_known_contract(contract_address):
             return  1
         else:
             return False
+
 #   在数据库中保存区块
 def save_block_to_db(block):
     count = 0
     for tx in block["transactions"]:
 	count = count +1
 	tx_type = -1
-	if not tx["to"]:
-            tx_type = 2 
-            contractAddr = helper.calculate_contract_address(tx)
-            insert_into_addr_args = [contractAddr,1]
-            cursor.execute(insert_into_addr , insert_into_addr_args);
-        else:
-            # Should check what the 'to' field is here (contract/person)
-            tx_type = 0
-            if is_known_contract(tx["to"]):
-            	tx_type = 1
-            insert_into_addr_args = [tx["to"],1]
-            cursor.execute(insert_into_addr , insert_into_addr_args)
-        try: 
-            gas =  int(tx["gas"])*int(tx["gasPrice"])
+	gas =  int(tx["gas"])*int(tx["gasPrice"])
+	try:
+		if not tx["to"]:
+            		tx_type = 2 
+            		contractAddr =str( helper.calculate_contract_address(tx))
+	    	
+			print "contractAddr=",contractAddr
+			print type(contractAddr) 
+			#print json.loads(block)
+           		insert_into_addr_args = [contractAddr,1]
+            		cursor.execute(insert_into_addr , insert_into_addr_args);
+	   		insert_into_addr_args = [tx["from"],0]
+                        cursor.execute(insert_into_addr , insert_into_addr_args);
+ 
+	   		insert_into_tx_args   = [ tx["blockNumber"],tx["blockHash"],tx["hash"],tx["from"],contractAddr,tx["transactionIndex"],tx["input"],tx["value"],tx_type ]
+            		cursor.execute(insert_into_Transactions,insert_into_tx_args)
 
-            insert_into_addr_args = [tx["from"],0]
-            cursor.execute(insert_into_addr , insert_into_addr_args)
+            		insert_into_addr_from  = [tx["blockNumber"],tx["hash"],tx["from"],tx["value"],gas,0]
+            		cursor.execute(insert_into_AddressHistory,insert_into_addr_from)
 
-            insert_into_addr_args = [tx["to"],0]
-            cursor.execute(insert_into_addr , insert_into_addr_args)
+	                insert_into_addr_to    = [tx["blockNumber"],tx["hash"],contractAddr,tx["value"],gas,1]
+           		cursor.execute(insert_into_AddressHistory,insert_into_addr_to)
 
-            insert_into_tx_args   = [ tx["blockNumber"],tx["blockHash"],tx["hash"],tx["from"],tx["to"],tx["transactionIndex"],tx["input"],tx["value"],tx_type ]
-            cursor.execute(insert_into_Transactions,insert_into_tx_args)
 
-            insert_into_addr_from  = [tx["blockNumber"],tx["hash"],tx["from"],tx["value"],gas,0]
-    	    insert_into_addr_to    = [tx["blockNumber"],tx["hash"],tx["to"],tx["value"],gas,1]
-       	    cursor.execute(insert_into_AddressHistory,insert_into_addr_from)
-            cursor.execute(insert_into_AddressHistory,insert_into_addr_to)
+       		else:
+            	# Should check what the 'to' field is here (contract/person)
+            		tx_type = 0
+            		if is_known_contract(tx["to"]):
+            			tx_type = 1
+            			insert_into_addr_args = [tx["to"],1]
+            			cursor.execute(insert_into_addr , insert_into_addr_args)
+            		insert_into_addr_args = [tx["to"],0]
+            		cursor.execute(insert_into_addr , insert_into_addr_args)
+	    		insert_into_addr_args = [tx["from"],0]
+            		cursor.execute(insert_into_addr , insert_into_addr_args)
+            		insert_into_tx_args   = [ tx["blockNumber"],tx["blockHash"],tx["hash"],tx["from"],tx["to"],tx["transactionIndex"],tx["input"],tx["value"],tx_type ]
+            		cursor.execute(insert_into_Transactions,insert_into_tx_args)
+            		insert_into_addr_from  = [tx["blockNumber"],tx["hash"],tx["from"],tx["value"],gas,0]
+       	    		cursor.execute(insert_into_AddressHistory,insert_into_addr_from)
+	    		insert_into_addr_to    = [tx["blockNumber"],tx["hash"],tx["to"],tx["value"],gas,1]
+            		cursor.execute(insert_into_AddressHistory,insert_into_addr_to)
         except Exception as e:
-	    print("ERROR",str(e))
+	    print("ERROR",str(e),tx)
 #	print tx
-    	insert_into_block_args = [block["number"],block["hash"],block["timestamp"],block["parentHash"],block["miner"],count,block["gasUsed"]]
-    	cursor.execute(insert_into_block , insert_into_block_args);
-    	if(block["number"] % FREQUENCY == 0):
-    	    try:
-                n=db.commit()
-         	print("Loading %d blocks to DB from #%s (at %s )"% (FREQUENCY,block["number"], time.asctime(time.localtime())  ))
-    	    except Exception as e:
-      		print("ERROR",str(e))
+    insert_into_block_args = [block["number"],block["hash"],block["timestamp"],block["parentHash"],block["miner"],count,block["gasUsed"]]
+    cursor.execute(insert_into_block , insert_into_block_args);
+    if(block["number"] % FREQUENCY == 0):
+    	try:
+            n=db.commit()
+            print("Loading %d blocks to DB from #%s (at %s )"% (FREQUENCY,block["number"], time.asctime(time.localtime())  ))
+    	except Exception as e:
+      	    print("ERROR",str(e))
 #   把区块、交易和账户等信息保存到数据库中
 def save_to_db(block):
     save_block_to_db(block);
